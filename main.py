179b72a8e48135e7e4174338ad97f824f4deda2a
Ryan A <alumkalryan@gmail.com>
2022-09-05 19:21:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ from discord.ext import commands, tasks
 
 client = discord.Client()
 
-
+bot = commands.Bot(command_prefix= '!')
 
 @client.event
 async def on_ready():
     print(f' {client.user} is online!')
-
+    
+@bot.command 
+async def hello(ctx, arg):
+    await ctx.send(arg)
+"""
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
+
+USE THIS:
+https://discordpy.readthedocs.io/en/stable/ext/commands/commands.html 
+    
+    
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
-  
+"""
 if __name__ == '__main__':
     client.run('OTk0NzYwODczNDU1Mzk0ODU3.GJfoH1.J4gNv6PFzlgSloBEek_4kw7WMhqxhl28kzsJNE')
