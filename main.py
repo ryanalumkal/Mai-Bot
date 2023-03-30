f1121309b4f582b5e02a47fa950e61747adef4ff
Ryan A <alumkalryan@gmail.com>
2023-03-30 12:22:27 -0400

    bot

--- main.py
+++ main.py
@@ -2,33 +2,28 @@
 import os
 import discord 
 from discord.ext import commands, tasks
+from dotenv import load_dotenv
 
+load_dotenv()
 
+intents = discord.Intents.default()
+intents.messages = True
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix= '!')
+ClientID = os.getenv('ClientID')
 
 @client.event
 async def on_ready():
-    print(f' {client.user} is online!')
-    
-@bot.command 
-async def hello(ctx, arg):
-    await ctx.send(arg)
-"""
+    print(f'We have logged in as {client.user}')
+
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    #if message.author == client.user:
+        #return
 
-
-USE THIS:
-https://discordpy.readthedocs.io/en/stable/ext/commands/commands.html 
-    
-    
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
-"""
+
 if __name__ == '__main__':
-    client.run('OTk0NzYwODczNDU1Mzk0ODU3.GJfoH1.J4gNv6PFzlgSloBEek_4kw7WMhqxhl28kzsJNE')
+    client.run(ClientID)
